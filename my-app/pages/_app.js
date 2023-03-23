9f74456dfc2873d8714413fd8158647da009d423
Max Bazler <112581957+maxbazler@users.noreply.github.com>
2023-03-22 20:50:43 -0700

    Delete my-app directory

--- my-app/pages/_app.js
+++
@@ -1,9 +0,0 @@
-import '@/styles/globals.css'
-import handler from '@/pages/api/hello'
-
-handler();
-
-export default function App({ Component, pageProps }) {
-    return <Component {...pageProps }
-    />
-}
